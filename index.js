56e156b9fddbe4f04e984ba5ae1e80bca6682320
sanjay <sanjaysulochanap@gmail.com>
2021-12-03 15:15:52 +0530

    CHANGED

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const Box = props => {
   //  Write your code here.
+  const {text, style} = props
+  return(
+  <div className={style} >
+    <h1 className="bheading">{text}</h1>
+    </div>
+  )
+
 }
 
 const element = (
   //  Write your code here. 
+  <div className="card">
+    <h1 className="heading">Boxes</h1>
+    <div className="boxCard">
+      <Box text="Small" style="smallBox"/>
+      <Box text="Medium" style="mediumBox"/>
+      <Box text="Large" style="largeBox"/>
+
+    </div>
+
+  </div>
 )
 
 ReactDOM.render(element, document.getElementById("root"));
